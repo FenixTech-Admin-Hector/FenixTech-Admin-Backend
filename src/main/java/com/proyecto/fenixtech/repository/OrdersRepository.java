7da71b30d0fa4d4caf20845238621e139e7ccc11
nestor <nestor.gonzalez@iesdoctorbalmis.com>
2026-03-03 10:40:31 +0100

    refactor: Refactorizada la API de Orders

--- src/main/java/com/proyecto/fenixtech/repository/OrdersRepository.java
+++ src/main/java/com/proyecto/fenixtech/repository/OrdersRepository.java
@@ -2,8 +2,10 @@ package com.proyecto.fenixtech.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.proyecto.fenixtech.model.Orders;
-import com.proyecto.fenixtech.model.enums.OrderStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,12 +13,25 @@ import java.util.List;
 
 public interface OrdersRepository extends JpaRepository<Orders, Integer> {
     List<Orders> findByBuyer_UserId(Integer id);
-    List<Orders> findByStatus(OrderStatus status);
-    List<Orders> findByTotalAmountGreaterThan(Double amount);
-    List<Orders> findByTotalAmountLessThan(Double amount);
-    List<Orders> findByRequiresShipping(Boolean requiresShipping);
-    List<Orders> findByOrderDateBetween(LocalDateTime start, LocalDateTime end);
-    List<Orders> findByStatusAndRequiresShipping(Boolean requiresShipping, OrderStatus status);
+    @Query(value = "SELECT * FROM orders WHERE " +
+           "(:minAmount IS NULL OR total_amount >= :minAmount) AND " +
+           "(:maxAmount IS NULL OR total_amount <= :maxAmount) AND " +
+           "(:minDate IS NULL OR order_date >= :minDate) AND " +
+           "(:maxDate IS NULL OR order_date <= :maxDate) AND " +
+           "(:status IS NULL OR status = :status) AND " +
+           "(:requiresShipping IS NULL OR requires_shipping = :requiresShipping)", 
+           nativeQuery = true
+          )
+    List<Orders> findByConditions(
+            @Param("minAmount") Double minAmount,
+            @Param("maxAmount") Double maxAmount,
+            @Param("minDate") LocalDateTime minDate,
+            @Param("maxDate") LocalDateTime maxDate,
+            @Param("status") String status,
+            @Param("requiresShipping") Boolean requiresShipping
+    );
+
+
 
 
 }
