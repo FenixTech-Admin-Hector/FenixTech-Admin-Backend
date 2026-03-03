c82b4ece429a6b775141bb4ea5e7ced24518df9d
Saabba21 <saba.ishkhneli@iesdoctorbalmis.com>
2026-03-03 13:05:28 +0100

    feat(api, service): se añade Posts, falta terminarlo

---
+++ src/main/java/com/proyecto/fenixtech/controller/PostsController.java
@@ -0,0 +1,78 @@
+package com.proyecto.fenixtech.controller;
+
+import org.springframework.data.domain.Page; 
+import org.springframework.data.domain.Pageable; 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.proyecto.fenixtech.model.Posts;
+import com.proyecto.fenixtech.service.PostsService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Posts", description = "API para gestión de posts")
+@RequestMapping("/api/posts")
+@RestController
+public class PostsController {
+    @Autowired
+    private PostsService postsService;
+
+    @Operation(summary = "Obtener todos los posts", description = "Devuelve una lista paginada de posts")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Número de posts obtenido con éxito"),
+            @ApiResponse(responseCode = "404", description = "No se encontraron posts")
+    })
+
+    @GetMapping
+    public ResponseEntity<Page<Posts>> findAllPosts(
+            @PageableDefault(page = 0, // Si no me dan página, dame la primera
+                    size = 10, // Si no me dan tamaño, dame 10
+                    sort = "createdAt", // Ordena por fecha
+                    direction = Sort.Direction.DESC // Los más nuevos primero
+            ) Pageable pageable) {
+
+        return ResponseEntity.ok(postsService.findAllPosts(pageable));
+    }
+
+    @Operation(summary = "Obtener post por ID", description = "Devuelve un post por su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Post obtenido con éxito"),
+            @ApiResponse(responseCode = "404", description = "Post no encontrado")
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<Posts> findPostById(@PathVariable Integer id) {
+        return ResponseEntity.ok(postsService.findById(id));
+    }
+
+
+    @Operation(summary = "Obtener posts por ID de usuario", description = "Devuelve una lista de posts asociada a un ID de usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Posts obtenidos con éxito"),
+            @ApiResponse(responseCode = "404", description = "No se encontraron posts para el usuario")
+    })
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Posts>> findByUserId(@PathVariable Integer userId) {
+        return ResponseEntity.ok(postsService.findByUserId(userId));
+    }
+
+    @Operation(summary = "Obtener posts recientes", description = "Devuelve una lista de los posts más recientes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Posts recientes obtenidos con éxito"),
+            @ApiResponse(responseCode = "404", description = "No se encontraron posts recientes")
+    })
+    @GetMapping("/recent")
+    public ResponseEntity<List<Posts>> findRecentPosts() {
+        return ResponseEntity.ok(postsService.findRecentPosts(5));
+    }
+
+}
---
+++ src/main/java/com/proyecto/fenixtech/service/PostsService.java
@@ -0,0 +1,46 @@
+package com.proyecto.fenixtech.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page; 
+import org.springframework.data.domain.Pageable; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.fenixtech.exception.ResourceNotFoundException;
+import com.proyecto.fenixtech.model.Posts;
+import com.proyecto.fenixtech.repository.PostsRepository;
+import com.proyecto.fenixtech.repository.UsersRepository;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PostsService {
+    @Autowired
+    private PostsRepository postsRepository;
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Transactional(readOnly = true)
+    public Page<Posts> findAllPosts(Pageable pageable){
+        return postsRepository.findAll(pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Posts findById(Integer id){
+        return postsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post no encontrado con id: " + id));
+    }
+
+     @Transactional(readOnly = true)
+     public List<Posts> findByUserId(Integer id){
+        usersRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado con id: " + id));
+        return postsRepository.findByAuthor_UserId(id);
+     }
+
+     @Transactional(readOnly = true)
+     public List<Posts> findRecentPosts(Integer limit) {
+        return postsRepository.findTop5ByOrderByCreatedAtDesc();
+     }
+}
