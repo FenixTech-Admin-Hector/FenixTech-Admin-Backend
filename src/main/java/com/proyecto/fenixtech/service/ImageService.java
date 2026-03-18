9532d243a5c6ffa036ba16bc4684c29ce30472c1
Néstor <gonzaleznestor846@gmail.com>
2026-03-18 13:51:21 +0100

    feat: subido el service de guardar iamgenes despues de hacer un post o un put

---
+++ src/main/java/com/proyecto/fenixtech/service/ImageService.java
@@ -0,0 +1,29 @@
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class ImageService {
+
+    private final Path root = Paths.get("/app/uploads");
+
+    public String guardarImagen(MultipartFile archivo) {
+        try {
+            if (!Files.exists(root)) {
+                Files.createDirectories(root);
+            }
+
+            String nombreUnico = UUID.randomUUID().toString() + "_" + archivo.getOriginalFilename();
+            
+            Files.copy(archivo.getInputStream(), this.root.resolve(nombreUnico));
+            
+            return nombreUnico;
+        } catch (IOException e) {
+            throw new RuntimeException("No se pudo guardar la imagen: " + e.getMessage());
+        }
+    }
+}
